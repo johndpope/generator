8c70c1a65967c06df72f88391c0791b69af5bc77
David Smiles <ugberodavid@gmail.com>
2021-03-15 18:23:51 +0000

    small changes

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app="generator"
 docker build -t ${app} .
-docker run -d -p 443:443 --name=${app} -v $PWD:/app -v $PWD/data/certs:/etc/nginx/certs ${app}
+docker run -d -p 80:80 --name=${app} -v $PWD:/app -v $PWD/data/certs:/etc/nginx/certs ${app}
